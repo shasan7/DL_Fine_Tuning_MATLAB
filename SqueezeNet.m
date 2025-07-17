24124b860e9832b2d2b7657ed03fc0b25098c9d7
Md. Intikhab Shahriar Hasan <45317472+shasan7@users.noreply.github.com>
2025-07-17 19:49:40 +0600

    Update SqueezeNet.m

--- SqueezeNet.m
+++ SqueezeNet.m
@@ -35,7 +35,7 @@ numIterationsPerEpoch = floor(numObservationsTrain/miniBatchSize); % Validation 
 options = trainingOptions('adam', 'MiniBatchSize', miniBatchSize, 'MaxEpochs', 20, 'InitialLearnRate', 1e-4, 'Shuffle', 'every-epoch', 'ValidationData', augTestImages, 'ValidationFrequency', numIterationsPerEpoch, 'Verbose', false, 'Plots', 'training-progress', 'OutputNetwork', 'best-validation');
 
 % training the SqueezeNet
-netTransfer = trainNetwork(augTrainImages, layers, options);
+netTransfer = trainNetwork(augTrainImages, lgraphSqz, options);
 
 % Classifying images
 YPred = classify(netTransfer, augTestImages);
